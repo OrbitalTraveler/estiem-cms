40a1bc2cefb19bc391e2d24cf2af5645a9288435
OrbitalTraveler <jevic.it56.2022@uns.ac.rs>
2024-10-24 11:15:10 +0200

    TODO or Not TODO, That is the Question

--- actions/reset-password.ts
+++ actions/reset-password.ts
@@ -2,6 +2,11 @@
 
 import * as z from "zod";
 
+// TODO: Create ResetSchema take a look at index.ts under schemas folder
+// ResetScheam should have an email field validation only
+// TODO: Create sendPasswordResetEmail function take a look at mail.ts under lib folder
+// Take a look at the mail.ts file to see an example of how to create a function that sends an email
+// for the password reset, dont bother making email templates. Just send the token in the email
 import { ResetSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
 import { sendPasswordResetEmail } from "@/lib/mail";
